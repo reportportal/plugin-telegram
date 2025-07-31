b8d6095dba9fb3dc0f29948c6b207203d4bdcd7c
APiankouski <109206864+APiankouski@users.noreply.github.com>
2025-07-31 16:24:16 +0300

    EPMRPP-104087 || Extra ':' symbol is displayed when attribute has only value (#7)

--- src/main/java/com/epam/reportportal/extension/telegram/collector/laucnh/AttributesCollector.java
+++ src/main/java/com/epam/reportportal/extension/telegram/collector/laucnh/AttributesCollector.java
@@ -7,7 +7,6 @@ import com.epam.reportportal.extension.telegram.collector.PropertyCollector;
 import com.epam.reportportal.extension.telegram.model.enums.template.DefaultTemplateProperty;
 import com.epam.ta.reportportal.entity.ItemAttribute;
 import com.epam.ta.reportportal.entity.launch.Launch;
-import java.util.Collections;
 import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -28,7 +27,10 @@ public class AttributesCollector implements PropertyCollector<Launch, DefaultTem
   private Map<DefaultTemplateProperty, String> convertToProperties(Set<ItemAttribute> attributes) {
     final String attributesString = attributes.stream()
         .filter(a -> BooleanUtils.isFalse(a.isSystem()))
-        .map(a -> ofNullable(a.getKey()).orElse("") + ":" + a.getValue())
+        .map(a -> {
+          String key = a.getKey();
+          return key != null ? key + ":" + a.getValue() : a.getValue();
+        })
         .collect(Collectors.joining("; "));
     return Map.of(LAUNCH_ATTRIBUTES, attributesString);
   }
